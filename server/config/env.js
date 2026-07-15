c52fe6ec403ffeb9cd8550aae5cac95441edd470
drish-kamble <drishya.kamble.t22019@sophiacollege@edu.in>
2026-07-15 20:07:48 +0530

    changes in server

--- server/config/env.js
+++ server/config/env.js
@@ -1,7 +1,4 @@
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({
-  path: path.resolve(process.cwd(), ".env"),
-  debug: true,
-});
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import("dotenv");
+  dotenv.config();
+}
