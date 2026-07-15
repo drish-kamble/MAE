1e92086bc56ba09e22d7851c822332bb87c4182f
drish-kamble <drishya.kamble.t22019@sophiacollege.edu.in>
2026-07-15 17:05:01 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://mae-3qhelnzul-drish-kambles-projects.vercel.app/";
+const API_BASE = "https://mae-3qhelnzul-drish-kambles-projects.vercel.app";
 
 /* ---------------- DEFAULT OPTIONS ---------------- */
 const defaultOptions = {
