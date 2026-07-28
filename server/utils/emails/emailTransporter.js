4a38db1f3b15c1d85a83955ef9c1648af6c24bd8
drish-kamble <drishya.kamble.t22019@sophiacollege@edu.in>
2026-07-28 13:30:21 +0530

    SMTP

--- server/utils/emails/emailTransporter.js
+++ server/utils/emails/emailTransporter.js
@@ -1,22 +1,41 @@
 import nodemailer from "nodemailer";
+import dns from "node:dns";
 import { EMAIL_CONFIG } from "../../config/emailConfig.js";
+
+dns.lookup("smtp-relay.brevo.com", (err, address, family) => {
+  console.log("DNS LOOKUP:");
+  console.log({
+    err,
+    address,
+    family,
+  });
+});
 
 export const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
+  requireTLS: true,
+
   auth: {
     user: EMAIL_CONFIG.user,
     pass: EMAIL_CONFIG.pass,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+
+  logger: true,
+  debug: true,
 });
 
-transporter.verify((err) => {
-  if (err) {
-    console.error("SMTP Error:", err);
-  } else {
-    console.log("✅ Brevo SMTP Ready");
+(async () => {
+  try {
+    await transporter.verify();
+    console.log("✅ SMTP VERIFIED");
+  } catch (err) {
+    console.error("❌ VERIFY FAILED");
+    console.error(err);
   }
-});
-console.log("EMAIL_USER:", EMAIL_CONFIG.user);
-console.log("SMTP HOST: smtp-relay.brevo.com");
+})();
