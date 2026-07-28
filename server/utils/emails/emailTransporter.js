eea63821ed4a8e82476f5b046ce5b52c4d63dfb0
drish-kamble <drishya.kamble.t22019@sophiacollege@edu.in>
2026-07-28 13:15:24 +0530

    Brevo SMTP

--- server/utils/emails/emailTransporter.js
+++ server/utils/emails/emailTransporter.js
@@ -2,26 +2,19 @@ import nodemailer from "nodemailer";
 import { EMAIL_CONFIG } from "../../config/emailConfig.js";
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false, // TLS via STARTTLS
+  secure: false,
   auth: {
     user: EMAIL_CONFIG.user,
     pass: EMAIL_CONFIG.pass,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  pool: true,
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
-transporter.verify((err, success) => {
+transporter.verify((err) => {
   if (err) {
     console.error("SMTP Error:", err);
   } else {
-    console.log("✅ SMTP Ready");
+    console.log("✅ Brevo SMTP Ready");
   }
 });
