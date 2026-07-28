fbe7c3bd976063e15462534d2583793d1076f5da
drish-kamble <drishya.kamble.t22019@sophiacollege@edu.in>
2026-07-28 12:56:29 +0530

    Emails

--- server/utils/emails/emailTransporter.js
+++ server/utils/emails/emailTransporter.js
@@ -2,9 +2,26 @@ import nodemailer from "nodemailer";
 import { EMAIL_CONFIG } from "../../config/emailConfig.js";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS via STARTTLS
   auth: {
     user: EMAIL_CONFIG.user,
     pass: EMAIL_CONFIG.pass,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  pool: true,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.error("SMTP Error:", err);
+  } else {
+    console.log("✅ SMTP Ready");
+  }
 });
